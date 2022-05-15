a71b025bbd45ae79948c1973146266e51c8c6955
Albert Mwasisoba <almwassy@gmail.com>
2022-05-16 00:59:06 +0300

    modified database connection

--- databaseConn.php
+++ databaseConn.php
@@ -18,21 +18,5 @@ if ($conn->query($sql) === TRUE) {
   echo "Error creating database: " . $conn->error;
 }
 
-// sql to create table
-$sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    phone VARCHAR(30),
-    pass VARCHAR(30) NOT NULL,
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-      echo "Table MyGuests created successfully";
-    } else {
-      echo "Error creating table: " . $conn->error;
-    }
-
 $conn->close();
 ?>
