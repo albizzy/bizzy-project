9ee1a10b743595a5b16556ea9684d129f308d29b
Albert Mwasisoba <almwassy@gmail.com>
2022-05-16 00:23:17 +0300

    modified the database connection

--- databaseConn.php
+++ databaseConn.php
@@ -1,8 +1,32 @@
 <?php
-    define("SERVER","localhost");
-    define("USER","root");
-    define("PASS","");
-    define("DB","bizzy-database");
+    define("SERVER", "localhost");
+    define("USER", "root");
+    define("PASS", "");
+    define("DB", "");
+    
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    $link_to_database=mysqli_connect(SERVER,USER,PASS,DB) or die();
+    // Check connection
+    if (!$conn) {
+      die("Connection failed: " . mysqli_connect_error());
+    }
+    
+    // sql to create table
+    $sql = "CREATE TABLE Users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    phone VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    pass VARCHAR(300) NOT NULL,
+    )";
+    
+    if (mysqli_query($conn, $sql)) {
+      echo "Table Users created successfully";
+    } else {
+      echo "Error creating table: " . mysqli_error($conn);
+    }
+    
+    mysqli_close($conn);
 ?>
